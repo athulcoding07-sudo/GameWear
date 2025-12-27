0bb81ee76ccbd7320c7ea534a6bb921fa980d9ef
athul <athul0752003@gmail.com>
2025-12-27 11:43:35 +0530

    feat(auth): add forgot password flow using OTP verification

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,7 +2,19 @@
 Docstring for gamewear.apps.users.urls
 """
 from django.urls import path
-from .views import login_view,signup_view,user_dashboard,verify_signup_otp,resend_signup_otp
+from .views import (
+    login_view,
+    signup_view,
+    user_dashboard,
+    verify_signup_otp,
+    resend_signup_otp,
+    forgot_password_view,
+    verify_reset_otp,
+    resend_reset_otp,
+    reset_password_view,
+    
+)
+#from .views import login_view,signup_view,user_dashboard,verify_signup_otp,resend_signup_otp,forgot_password_view,verify_reset_otp
 
 
 app_name = "users"
@@ -13,8 +25,10 @@ urlpatterns = [
     path("dashboard/",user_dashboard,name="dashboard"),
     path("verify-signup-otp/", verify_signup_otp, name="verify_signup_otp"),
     path("resend-signup-otp/", resend_signup_otp, name="resend_signup_otp"),
-
-
+    path("forgot-password/", forgot_password_view, name="forgot_password"),
+    path("verify-reset-otp/", verify_reset_otp, name="verify_reset_otp"),
+    path("resend-reset-otp/", resend_reset_otp, name="resend_reset_otp"),
+    path("reset_password/", reset_password_view, name="reset_password"),
 
 
 ]
